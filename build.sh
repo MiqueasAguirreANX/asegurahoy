6f3168a967f2e53149945945999d784332ffbc0a
MiqueasAguirreANX <70060960+MiqueasAguirreANX@users.noreply.github.com>
2022-12-02 20:56:09 -0300

    little fix

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ set -o errexit
 
 poetry install
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+python3 -m pip install --upgrade pip setuptools wheel
+python3 manage.py collectstatic --no-input
+python3 manage.py migrate
