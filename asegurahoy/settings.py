9ef5da3f192d27770018e320900caba3053f8ac1
MiqueasAguirreANX <70060960+MiqueasAguirreANX@users.noreply.github.com>
2023-05-11 22:02:46 -0300

    fixing whitenoise

--- asegurahoy/settings.py
+++ asegurahoy/settings.py
@@ -119,9 +119,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# if not DEBUG:
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
